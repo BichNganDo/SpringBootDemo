993a1b1a68080e54891d1b30715cd4a2efbef9cb
Ngan Do <ngando6598@gmail.com>
2021-12-27 10:33:47 +0700

    Hiển thị list data trong thymeleaf

--- src/main/java/com/example/hello1/controller/HomeController.java
+++ src/main/java/com/example/hello1/controller/HomeController.java
@@ -1,13 +1,29 @@
 package com.example.hello1.controller;
 
+import com.example.hello1.model.Person;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping
+    private static List<Person> persons = new ArrayList<Person>();
+    static {
+        persons.add(new Person("Ngan", "Do"));
+        persons.add(new Person("Luc", "Tran"));
+    }
+
+    @RequestMapping(value = {"/personList"}, method = RequestMethod.GET)
+    public String personList(Model model) {
+        model.addAttribute("persons", persons);
+        return "personList";
+    }
+
+    @RequestMapping("/")
     public String welcome(final Model model) {
         model.addAttribute("message", "hello");
         return "index";
