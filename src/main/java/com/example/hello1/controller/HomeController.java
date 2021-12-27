6f7599e792a7ba3233b12d3808850cfa49285029
Ngan Do <ngando6598@gmail.com>
2021-12-27 11:03:01 +0700

    Add Person

--- src/main/java/com/example/hello1/controller/HomeController.java
+++ src/main/java/com/example/hello1/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,4 +29,26 @@ public class HomeController {
         model.addAttribute("message", "hello");
         return "index";
     }
+
+    @RequestMapping(value = {"/addPerson"}, method = RequestMethod.GET)
+    public String showAddPersonPage(Model model) {
+        Person personForm = new Person();
+        model.addAttribute("personForm", personForm);
+
+        return "addPerson";
+    }
+
+    @RequestMapping(value = {"/addPerson"}, method = RequestMethod.POST)
+    public String savePerson(Model model, @ModelAttribute("personForm") Person personForm) {
+        String firstName = personForm.getFirstName();
+        String lastName = personForm.getLastName();
+        if (firstName != null && firstName.length() > 0
+                && lastName != null && lastName.length() > 0) {
+            persons.add(personForm);
+            return "redirect:/personList";
+        }
+        model.addAttribute("errorMessage", "First Name and Last Name are requied!");
+        return "addPerson";
+    }
+
 }
